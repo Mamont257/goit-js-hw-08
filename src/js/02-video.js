9aa706b338b8d0ca9e7e0247cbab724535a1849f
Volodymyr Zankevych <108954365+Mamont257@users.noreply.github.com>
2022-12-15 23:59:26 +0200

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Vimeo from "@vimeo/player";
 
+const iframe = document.querySelector('#vimeo-player');
+// console.log(iframe);
+const player = new Vimeo(iframe);
+// console.log(player);
+
+player.on('timeupdate', function(data) {
+    // console.log('played the video!');
+    console.dir(data.seconds);
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
