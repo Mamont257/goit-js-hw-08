74ab1bc6cab0513768cc5263745cc3fdb8eeff11
Volodymyr Zankevych <108954365+Mamont257@users.noreply.github.com>
2022-12-15 23:07:52 +0200

    task1+-done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,19 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+console.log(SimpleLightbox);
 console.log(galleryItems);
+
+
+const ul = document.querySelector('.gallery');
+
+const markup = galleryItems.reduce((acc,{ preview, original, description }) => acc + 
+`<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" title="${description}" />
+ </a>`, '')
+
+ul.insertAdjacentHTML('beforeend', markup);
+
+new SimpleLightbox('.gallery a', { captionDelay: 250, overlayOpacity: 0.5 });
