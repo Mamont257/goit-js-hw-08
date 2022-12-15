06a672b12945490fa5e7ba08db32fb792cd0701e
Volodymyr Zankevych <108954365+Mamont257@users.noreply.github.com>
2022-12-15 23:17:54 +0200

    task1-done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,6 @@
 // Add imports above this line
 import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
@@ -17,3 +18,10 @@ const markup = galleryItems.reduce((acc,{ preview, original, description }) => a
 ul.insertAdjacentHTML('beforeend', markup);
 
 new SimpleLightbox('.gallery a', { captionDelay: 250, overlayOpacity: 0.5 });
+
+
+// 1. Додай бібліотеку SimpleLightbox як залежність проекту, використовуючи npm 
+// (посилання на CDN з твоєї минулої роботи більше не потрібне).
+// 2. Використовуй свій JavaScript код з попередньої домашньої роботи, 
+// але виконай рефакторинг з урахуванням того, що бібліотека була встановлена через npm (синтаксис import/export).
+// Для того щоб підключити CSS код бібліотеки в проект, необхідно додати ще один імпорт, крім того, що описаний в документації.
