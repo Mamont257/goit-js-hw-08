9c4cb69cd49d351e04650cfe33a473c73050e3ac
Mamont257 <108954365+Mamont257@users.noreply.github.com>
2022-12-16 16:03:15 +0200

    Почав робити 3 таск

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.feedback-form');
+form.addEventListener("submit", onSubmit);
+const obj = {};
 
+function onSubmit(evt) {
+    // evt.preventDefault();
+    const { email, message } = evt.currentTarget;
+
+    // email.value = "efffe";
+    // console.dir(email.value);
+    // console.log(message.value);
+
+    obj.email = email.value;
+    obj.message = message.value;
+    // console.log(obj);
+    localStorage.setItem("feedback-form-state", JSON.stringify(obj));
+}
+
+
+// console.log(obj);
+
+// console.dir(form.children[0].tex);
