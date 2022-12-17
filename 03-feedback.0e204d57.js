8302808892001089ab5e0c1effbd308e0ba43a2a
Mamont257 <108954365+Mamont257@users.noreply.github.com>
2022-12-17 10:42:56 +0000

    Deploying to gh-pages from @ Mamont257/goit-js-hw-08@a5c398ed1c861211bef558a1ba43483aee627d22 🚀

--- 03-feedback.0e204d57.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".feedback-form").addEventListener("submit",(function(t){const{email:a,message:s}=t.currentTarget;e.email=a.value,e.message=s.value,localStorage.setItem("feedback-form-state",JSON.stringify(e))}));const e={};
-//# sourceMappingURL=03-feedback.0e204d57.js.map
--- 03-feedback.8fae4788.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector(".feedback-form").addEventListener("submit",(function(a){var t=a.currentTarget,r=t.email,m=t.message;e.email=r.value,e.message=m.value,localStorage.setItem("feedback-form-state",JSON.stringify(e))}));var e={}}();
-//# sourceMappingURL=03-feedback.8fae4788.js.map
