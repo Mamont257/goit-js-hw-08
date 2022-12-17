2bb4db3127c65ef5812c99dce023375b05536b0a
Mamont257 <108954365+Mamont257@users.noreply.github.com>
2022-12-17 13:34:09 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -16,7 +16,6 @@ if (!memory) {
     message.value = currentValue.message;
 }
 
-
 function onInput(evt) {
     if (evt.target.name === "email") {
         obj.email = evt.target.value.trim();
@@ -36,49 +35,6 @@ function onSubmit(evt) {
     localStorage.clear();
     evt.currentTarget.reset()
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function onSubmit(evt) {
-//     // evt.preventDefault();
-//     const { email, message } = evt.currentTarget;
-
-//     // email.value = "efffe";
-//     // console.dir(email.value);
-//     // console.log(message.value);
-
-//     obj.email = email.value;
-//     obj.message = message.value;
-//     // console.log(obj);
-//     localStorage.setItem("feedback-form-state", JSON.stringify(obj));
-// }
-
-
-// console.log(obj);
-
-// console.dir(form.children[0].tex);
-
 
 
 
